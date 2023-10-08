ad18b3f493c5bdb5700141794f001db9fce384b7
firas98alashhab <127398270+firas98alashhab@users.noreply.github.com>
2023-10-08 15:48:34 +0300

    UPDATE

--- wp-content/themes/dexai/assets/js/main.js
+++ wp-content/themes/dexai/assets/js/main.js
@@ -22,32 +22,32 @@ $(window).on('load', function () {
 	=    		Mobile Menu			      =
 =============================================*/
 //SubMenu Dropdown Toggle
-// if ($('.menu-area li.menu-item-has-children ul').length) {
-// 	$('.menu-area .navigation li.menu-item-has-children').append('<div class="dropdown-btn"><span class="fas fa-angle-down"></span></div>');
+if ($('.menu-area li.menu-item-has-children ul').length) {
+	$('.menu-area .navigation li.menu-item-has-children').append('<div class="dropdown-btn"><span class="fas fa-angle-down"></span></div>');
 
-// }
+}
 
-// //Mobile Nav Hide Show
-// if ($('.mobile-menu').length) {
+//Mobile Nav Hide Show
+if ($('.mobile-menu').length) {
 
-// 	var mobileMenuContent = $('.menu-area .main-menu').html();
-// 	$('.mobile-menu .menu-box .menu-outer').append(mobileMenuContent);
+	var mobileMenuContent = $('.menu-area .main-menu').html();
+	$('.mobile-menu .menu-box .menu-outer').append(mobileMenuContent);
 
-// 	//Dropdown Button
-// 	$('.mobile-menu li.menu-item-has-children .dropdown-btn').on('click', function () {
-// 		$(this).toggleClass('open');
-// 		$(this).prev('ul, .tg-mega-menu-wrap').slideToggle(300);
-// 	});
-// 	//Menu Toggle Btn
-// 	$('.mobile-nav-toggler').on('click', function () {
-// 		$('body').addClass('mobile-menu-visible');
-// 	});
+	//Dropdown Button
+	$('.mobile-menu li.menu-item-has-children .dropdown-btn').on('click', function () {
+		$(this).toggleClass('open');
+		$(this).prev('ul, .tg-mega-menu-wrap').slideToggle(300);
+	});
+	//Menu Toggle Btn
+	$('.mobile-nav-toggler').on('click', function () {
+		$('body').addClass('mobile-menu-visible');
+	});
 
-// 	//Menu Toggle Btn
-// 	$('.menu-backdrop, .mobile-menu .close-btn').on('click', function () {
-// 		$('body').removeClass('mobile-menu-visible');
-// 	});
-// }
+	//Menu Toggle Btn
+	$('.menu-backdrop, .mobile-menu .close-btn').on('click', function () {
+		$('body').removeClass('mobile-menu-visible');
+	});
+}
 
 
 /*=============================================
