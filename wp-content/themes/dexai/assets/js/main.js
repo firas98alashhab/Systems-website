01ecf213836ac15721dab69e50065469eb88ec79
firas98alashhab <127398270+firas98alashhab@users.noreply.github.com>
2023-10-08 14:55:05 +0300

    update

--- wp-content/themes/dexai/assets/js/main.js
+++ wp-content/themes/dexai/assets/js/main.js
@@ -22,32 +22,34 @@ $(window).on('load', function () {
 	=    		Mobile Menu			      =
 =============================================*/
 //SubMenu Dropdown Toggle
-if ($('.menu-area li.menu-item-has-children ul').length) {
-	$('.menu-area .navigation li.menu-item-has-children').append('<div class="dropdown-btn"><span class="fas fa-angle-down"></span></div>');
+// Ensure the DOM is ready
+$(document).ready(function () {
+  if ($('.menu-area li.menu-item-has-children ul').length) {
+    $('.menu-area .navigation li.menu-item-has-children').append('<div class="dropdown-btn"><span class="fas fa-angle-down"></span></div>');
+  }
 
-}
+  if ($('.mobile-menu').length) {
+    var mobileMenuContent = $('.menu-area .main-menu').html();
+    $('.mobile-menu .menu-box .menu-outer').append(mobileMenuContent);
 
-//Mobile Nav Hide Show
-if ($('.mobile-menu').length) {
+    // Event delegation for dropdown button
+    $('.mobile-menu').on('click touchstart', 'li.menu-item-has-children .dropdown-btn', function (event) {
+      event.preventDefault();
+      $(this).toggleClass('open');
+      $(this).prev('ul, .tg-mega-menu-wrap').slideToggle(300);
+    });
 
-	var mobileMenuContent = $('.menu-area .main-menu').html();
-	$('.mobile-menu .menu-box .menu-outer').append(mobileMenuContent);
+    // Event delegation for menu toggle button
+    $('.mobile-nav-toggler').on('click touchstart', function () {
+      $('body').addClass('mobile-menu-visible');
+    });
 
-	//Dropdown Button
-	$('.mobile-menu li.menu-item-has-children .dropdown-btn').on('click', function () {
-		$(this).toggleClass('open');
-		$(this).prev('ul, .tg-mega-menu-wrap').slideToggle(300);
-	});
-	//Menu Toggle Btn
-	$('.mobile-nav-toggler').on('click', function () {
-		$('body').addClass('mobile-menu-visible');
-	});
-
-	//Menu Toggle Btn
-	$('.menu-backdrop, .mobile-menu .close-btn').on('click', function () {
-		$('body').removeClass('mobile-menu-visible');
-	});
-}
+    // Event delegation for closing the mobile menu
+    $('.menu-backdrop, .mobile-menu .close-btn').on('click touchstart', function () {
+      $('body').removeClass('mobile-menu-visible');
+    });
+  }
+});
 
 
 /*=============================================
