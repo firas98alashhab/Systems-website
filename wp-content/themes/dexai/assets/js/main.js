5b88df26c8a728c30332c8fe2f5ecb1a9fbdea81
firas98alashhab <127398270+firas98alashhab@users.noreply.github.com>
2023-10-09 12:36:15 +0300

    Update main.js

--- wp-content/themes/dexai/assets/js/main.js
+++ wp-content/themes/dexai/assets/js/main.js
@@ -24,30 +24,31 @@ $(window).on('load', function () {
 //SubMenu Dropdown Toggle
 if ($('.menu-area li.menu-item-has-children ul').length) {
 	$('.menu-area .navigation li.menu-item-has-children').append('<div class="dropdown-btn"><span class="fas fa-angle-down"></span></div>');
-
 }
 
-//Mobile Nav Hide Show
+// Mobile Nav Hide Show
 if ($('.mobile-menu').length) {
+	// Clone the main navigation menu
+	var mobileMenuContent = $('.menu-area .navigation').clone();
+	$('.mobile-menu .menu-box').append(mobileMenuContent);
 
-	var mobileMenuContent = $('.menu-area .main-menu').html();
-	$('.mobile-menu .menu-box .menu-outer').append(mobileMenuContent);
-
-	//Dropdown Button
+	// Dropdown Button
 	$('.mobile-menu li.menu-item-has-children .dropdown-btn').on('click', function () {
-		$(this).toggleClass('open');
-		$(this).prev('ul, .tg-mega-menu-wrap').slideToggle(300);
-	});
-	//Menu Toggle Btn
-	$('.mobile-nav-toggler').on('click', function () {
-		$('body').addClass('mobile-menu-visible');
+			$(this).toggleClass('open');
+			$(this).siblings('ul').slideToggle(300);
 	});
 
-	//Menu Toggle Btn
+	// Menu Toggle Btn
+	$('.mobile-nav-toggler').on('click', function () {
+			$('body').addClass('mobile-menu-visible');
+	});
+
+	// Menu Toggle Btn
 	$('.menu-backdrop, .mobile-menu .close-btn').on('click', function () {
-		$('body').removeClass('mobile-menu-visible');
+			$('body').removeClass('mobile-menu-visible');
 	});
 }
+
 
 
 /*=============================================
